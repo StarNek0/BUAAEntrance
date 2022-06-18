10b34cf6cf161dd38d8a5210e1cd6b884e3bc746
stardrewer <stardrewer@gmail.com>
2022-06-18 17:51:33 +0800

    feat(LinearList): delete and search

--- DataStruct/LinearList.cpp
+++ DataStruct/LinearList.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int MaxSize = 10;
 typedef int ElemType;
 
-// p22 在长度为n的线性表A的第i个位置插入一个新元素item
+// <数据结构教程>p22 在长度为n的线性表A的第i个位置插入一个新元素item
 bool insert_list(ElemType A[], int& n, int i, ElemType item) {
 	if (n == MaxSize || i < 1 || i > n + 1)
 		return false;
@@ -15,7 +15,6 @@ bool insert_list(ElemType A[], int& n, int i, ElemType item) {
 	n++;
 	return true;
 }
-
 void insert_list_test() {
 	ElemType A[MaxSize] = { 1,2,3 };
 	int n = 3;
@@ -23,8 +22,35 @@ void insert_list_test() {
 	printf("%d %d %d %d %d\n", A[0], A[1], A[2], A[3], A[4]);
 }
 
+// <数据结构教程>p23 删除第i个元素
+bool delete_list(ElemType A[], int& n, int i) {
+	if (i<1 || i > n)
+		return false;
+	for (int j = i - 1; j < n; j++)
+		A[j] = A[j + 1];
+	n--;
+	return true;
+}
+void delete_list_test() {
+	ElemType A[MaxSize] = { 1, 2, 3, 4, 5, 6 };
+	int n = 6;
+	delete_list(A, n, 2);
+	printf("%d %d %d", A[0], A[1], A[2]);
+}
 
+// <数据结构教程>p24 查找
+int locate(ElemType A[], int n, ElemType elem) {
+	for (int i = 0; i < n; i++)
+		if (A[i] == elem)
+			return i + 1;
+	return -1;
+}
+void locate_test() {
+	ElemType A[MaxSize] = { 1, 2, 3, 4, 5, 6 };
+	int n = 6;
+	cout << locate(A, n, 3) << endl;
+}
 
 int main() {
-	insert_list_test();
+	locate_test();
 }
