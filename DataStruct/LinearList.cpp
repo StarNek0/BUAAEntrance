a877bafa2857c0a223c6db65906ea2d12eff9d26
stardrewer <stardrewer@gmail.com>
2022-06-20 16:22:59 +0800

    feat(DS): finished LinearList

--- DataStruct/LinearList.cpp
+++ DataStruct/LinearList.cpp
@@ -1,3 +1,6 @@
+/*
+	线性表的顺序存储结构
+*/
 #include<cstdio>
 #include<iostream>
 using namespace std;
@@ -51,27 +54,29 @@ void LocateTtest() {
 	cout << Locate(A, n, 3) << endl;
 }
 
-// <数据结构教程>p25 去重
+// <数据结构教程>p25 去重 O(n^2)
 void Purge(ElemType A[], int& n) {
-	//int i = 0, j;
-	//while (i < n) {
-	//	j = i + 1;
-	//	while (j < n) {
-	//		if (A[i] == A[j])
-	//			DeleteList(A, n, j + 1);
-	//		else
-	//			j++;
-	//	}
-	//	i++;
-	//}
-	for (int i = 0; i < n; i++)
-		for (int j = i + 1; j < n; j++)
+	int i = 0, j;
+	while (i < n) {
+		j = i + 1;
+		while (j < n) {
 			if (A[i] == A[j])
 				DeleteList(A, n, j + 1);
+			else
+				j++;
+		}
+		i++;
+	}
+	// 下面这种写法是错误的
+	// 因为j不应该无条件++: 执行DeleteList的这个分支不应该++
+	//for (int i = 0; i < n; i++)
+	//	for (int j = i + 1; j < n; j++)
+	//		if (A[i] == A[j])
+	//			DeleteList(A, n, j + 1);
 }
 void PurgeTest() {
-	ElemType A[MaxSize] = { 1, 2, 3, 3, 5, 6, 7, 6, 1 };
-	int n = 9;
+	ElemType A[MaxSize] = { 1, 2, 3, 3, 3, 5, 6, 7, 6, 1 };
+	int n = 10;
 	Purge(A, n);
 	for (int i = 0; i < n; i++)
 		cout << A[i] << " ";
@@ -89,7 +94,7 @@ void SelectSort(ElemType A[], int n) {
 				min_index = j;
 			}
 		}
-		/* 
+		/*
 			因为 min_value 初始值是A[i]
 			所以如果后n - 1 + 1个元素都比A[i]大
 			那么最后 min_value 不变, 相当于自己和自己交换, 不影响结果正确性
@@ -108,6 +113,92 @@ void SelectSortTest() {
 	cout << endl;
 }
 
+// <数据结构教程>p26 删除等于item的元素 O(n^2)
+void DeleteItem1(ElemType A[], int& n, ElemType item) {
+	for (int i = 0; i < n;)
+		if (A[i] == item)
+			DeleteList(A, n, i + 1);
+		else
+			i++;
+}
+void DeleteItem1Test() {
+	ElemType A[MaxSize] = { 1, 2, 3, 3, 3, 5, 6, 7, 6, 1 };
+	int n = 10;
+	DeleteItem1(A, n, 3);
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+}
+
+// <数据结构教程>p26 删除等于item的元素 O(n)
+void DeleteItem2(ElemType A[], int& n, ElemType item) {
+	int  k = -1;
+	for (int i = 0; i < n; i++) {
+		if (A[i] == item)
+			k++;
+		else
+			A[i - k - 1] = A[i];
+		n = n - k - 1;
+	}
+}
+void DeleteItem2Test() {
+	ElemType A[MaxSize] = { 1, 2, 3, 3, 3, 5, 6, 7, 6, 1 };
+	int n = 10;
+	DeleteItem2(A, n, 3);
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+}
+
+// <数据结构教程>p27 去重 O(n)
+void Purge1(ElemType A[], int& n) {
+	if (n == 1) {
+		return;
+	}
+	int i = 0;
+	for (int j = 1; j < n - 1; j++)
+		if (A[i] != A[j])
+			A[++i] = A[j];
+	n = i + 1;
+}
+void Purge1Test() {
+	ElemType A[MaxSize] = { 1, 2, 3, 3, 3, 5, 6, 7, 6, 1 };
+	int n = 10;
+	Purge1(A, n);
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+}
+
+// <数据结构教程>p27 非递减的线性表A[], 插入元素item并保持非递减
+bool InsertItem(ElemType A[], int& n, ElemType item) {
+	if (n == MaxSize)
+		return false;
+
+	if (item >= A[n - 1]) {
+		A[n] = item;
+		n++;
+	}
+	else {
+		int i = 0;
+		while (item >= A[i])
+			i++;
+		InsertList(A, n, i + 1, item);
+	}
+
+	return true;
+}
+void InsertItemTest() {
+	ElemType A[MaxSize] = { 1,2,3 };
+	int n = 3;
+	InsertItem(A, n, 2);
+	InsertItem(A, n, 9999);
+	for (int i = 0; i < n; i++)
+		cout << A[i] << " ";
+	cout << endl;
+}
+
+
 int main() {
-	SelectSortTest();
+	InsertItemTest();
 }
